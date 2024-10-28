4922bdc97124bb2458f54c0b3a9abc57c37e4320
Maxim <Timoxa@mail.ru>
2024-10-28 11:48:31 +0300

    second

--- Program.cs
+++ Program.cs
@@ -1,73 +1,48 @@
 ﻿//try
 //{
-    StreamWriter sw1 = new StreamWriter("C:\\Users/prdb/Desktop/JDJD/task8/Ввод.txt");
-    Console.WriteLine("Введите пример '+','-','/','*'");
-    Console.WriteLine("Пример x + y");
-    string str = Console.ReadLine();
-    sw1.WriteLine(str);
-    sw1.Close();
-    StreamWriter sw = new StreamWriter("C:\\Users/prdb/Desktop/JDJD/task8/Вывод.txt");
-    StreamReader sr = new StreamReader("C:\\Users/prdb/Desktop/JDJD/task8/Ввод.txt");
-    string[] input = sr.ReadLine().Split(" ");
-    int[] znak = new int[input.Length/2];
-    string[] answmas = new string[input.Length];
-    int answ = 0;
-    int del = 0;
-    int umn = 0;
-    int x = 0;
-        for (int i = 0;i < input.Length; i++)
-        {
-        switch (input[i])
-        {
-            case "+":
-                znak[x] = i ;
-                x++;
-                break;
-            case "-":
-                znak[x] = i ;
-                x++;
-                break;
-            case "/":
-                znak[x] = i ;
-                x++;
-                break;
-            case "*":
-                znak[x] = i ;
-                x++;
-                break;
-        }
-    }  
-
-    
-    
-    for (int k = znak.Length; k > 0; k--)
-    {
-   for (int j = 0; j < znak.Length; j++)
-    {
-        for (int i = 0; i < znak.Length; i++)
-        {
-            if (input[znak[i]] == "/" || input[znak[i]] == "*")
-            {
-                answmas[j] = input[znak[i]-1];
-                answmas[j+1] = input[znak[i]+1];
-            }
-            if (input[znak[i]] == "+" || input[znak[i]] == "-")
-            {
-                answmas[k] = input[znak[i]-1];
-                answmas[k + 1] = input[znak[i]+1];
-            }
-        }
-    }     
-}
-
-   foreach (string i in answmas)
+StreamWriter sw1 = new StreamWriter("C:\\Users/Максим/Desktop/1.11/task8-1.11/Ввод.txt");
+Console.WriteLine("Введите пример '+','-','/','*'");
+Console.WriteLine("Пример x + y");
+string str = Console.ReadLine();
+sw1.WriteLine(str);
+sw1.Close();
+StreamWriter sw = new StreamWriter("C:\\Users/Максим/Desktop/1.11/task8-1.11/Вывод.txt");
+StreamReader sr = new StreamReader("C:\\Users/Максим/Desktop/1.11/task8-1.11/Ввод.txt");
+string[] input = sr.ReadLine().Split(" ");
+string[] answmas = new string[input.Length-1];
+int answ = 0;
+string a = "";
+int x = 0;
+int k =1;
+int j = answmas.Length - 1;
+while (k<input.Length)
 {
-    Console.WriteLine(i);
+    if (input[k] == "/" || input[k] == "*")
+    {
+        answmas[x] = input[k - 1];
+        answmas[x + 1] = input[k + 1];
+        x += 2;  
+    }
+    k += 2;
+    
 }
-    
-   // Console.WriteLine(answ);
-   // sw.WriteLine(answ);
-    
+k = 1;
+while (k < input.Length)
+{
+    if (input[k] == "-" || input[k] == "+")
+    {
+        answmas[j - 1] = input[k - 1];
+        answmas[j] = input[k + 1];
+        j -= 2;
+    }
+    k += 2;
+}
+
+foreach (string n in answmas)
+{
+    Console.WriteLine(n);
+}
+
     sw.Close();
     sr.Close();
 //}
